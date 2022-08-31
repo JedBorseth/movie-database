13e3e5a5c7df948f5af983851c4d65f01d572f9e
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-08-31 15:20:24 -0700

    Styled the login page slightly

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,28 +1,47 @@
 import Header from "../components/Header";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect } from "react";
+import Image from "next/image";
+import NomiLogo from "../public/images/nomi-logo-white.svg";
+import MovieLogo from "../public/images/tmdb-logo.svg";
+import Button from '@mui/material/Button';
+import LoginIcon from '@mui/icons-material/Login'
+
 const Login = () => {
   const { data: session } = useSession();
   return (
     <div className="wrapper">
       <Header highlighted="login" />
       <main className="login">
+        <Image
+            src={NomiLogo}
+            alt="Nomi Movies Logo"
+            width={175}
+            height={175}
+          />
+        <h1>Please Login to View Your Favourites</h1>
         {session ? (
-          <button
+          <Button
             onClick={() => {
               signOut();
             }}
+            variant="contained" 
+            endIcon={<LoginIcon />}
+            className="login-button"
           >
             Sign Out
-          </button>
+          </Button>
         ) : (
-          <button
+          <Button
             onClick={() => {
               signIn();
             }}
+            variant="contained" 
+            endIcon={<LoginIcon />}
+            className="login-button"
           >
             Sign In
-          </button>
+          </Button>
         )}
       </main>
     </div>
