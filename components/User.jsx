3936adb5e9467b69d27e604de4e198e4042b9f05
JalenCameron <109976308+JalenCameron@users.noreply.github.com>
2022-09-01 13:09:59 -0700

    Made some changes to the search and login pages

--- components/User.jsx
+++ components/User.jsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 const User = () => {
   const { data: session } = useSession();
   return (
-    <div className="user">
+    <>
       {session?.user?.image ? (
-        <Image
-          src={session?.user?.image}
-          alt={session?.user?.name}
-          width="100"
-          height="100"
-        />
+        <div className="user">
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width="100"
+            height="100"
+          />
+        </div>
       ) : null}
-    </div>
+    </>
   );
 };
 
