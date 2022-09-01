11b7ea0c083cdf4db3fe45ddef18a0e112540780
JedBorseth <jedborseth@gmail.com>
2022-09-01 11:14:43 -0700

    account page functionality

--- components/User.jsx
+++ components/User.jsx
@@ -1,10 +1,17 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const User = () => {
   const { data: session } = useSession();
+  const router = useRouter();
   return (
-    <div className="user">
+    <div
+      className="user"
+      onClick={() => {
+        router.push("/account");
+      }}
+    >
       {session?.user?.image ? (
         <Image
           src={session?.user?.image}
