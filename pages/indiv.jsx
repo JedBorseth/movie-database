a212658a74091d8ad7d659ba589259f8bc9d44e5
Dandazine <ddaneshfar@my.bcit.ca>
2022-09-08 09:30:24 -0700

    trying to figure out cast query

--- pages/indiv.jsx
+++ pages/indiv.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 function Indiv() {
   // Using next js link to link movie prop to indiv pageXOffset. Router query received the info props and can be used for the movie info. Router query includes IdleDeadline, title, poster ,rating, release, overview
+
+  const [cast, setCast] = useState([]);
   const router = useRouter();
-  const data = router.query;
+  const castQuery = `https://api.themoviedb.org/3/movie/${router.query.id}/credits?api_key=565e5a5d8e336b7cee4dc5ea476e08f6&language=en-US`;
+
+  useEffect(() => {
+    const getCast = async () => {
+      const fetchAPI = await fetch(castQuery);
+      const castData = await fetchAPI.json();
+      setCast(castData);
+    };
+    getCast();
+  }, []);
 
   return (
     <div className="wrapper">
@@ -15,10 +27,10 @@ function Indiv() {
       <main>
         <div className="indiv-container">
           {/* Movie Poster  */}
-            <div className="movie-poster">
-              <img src={router.query.poster} alt={router.query.title} />
-            </div>
-          
+          <div className="movie-poster">
+            <img src={router.query.poster} alt={router.query.title} />
+          </div>
+
           {/* Movie Details  */}
           <section className="indiv-details">
             {/* Title  */}
@@ -36,16 +48,6 @@ function Indiv() {
                 <h2>Cast</h2>
                 <button>View All</button>
               </div>
-              {/* Cast 1
-              <div className="cast-info">
-                <img src={cast_pic} alt={cast_name} className="cast-member" />
-              </div>
-              <p>{cast_name}</p>
-              {/* Cast 2  */}
-              {/* <div className="cast-info">
-                <img src={cast_pic} alt={cast_name} className="cast-member" />
-              </div>
-              <p>{cast_name}</p> */}{" "}
             </div>
           </section>
         </div>
