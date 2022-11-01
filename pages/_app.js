f354a2daa973ec082db229c88c1fcf5188090e68
Jed Borseth <jbor@pop-os.localdomain>
2022-11-01 09:13:48 -0700

    adding vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import styles from "../styles/sass/styles.scss";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
